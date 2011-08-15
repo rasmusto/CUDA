b486a2fd37deb3eec666ae580e4feddde4708446
Neeraja Budamagunta <neeraja@meakin.cs.pdx.edu>
2011-08-15 10:32:07 -0700

    bug free version with performance measures

--- jacobi_final/original_jacobi6.cu
+++ jacobi_final/original_jacobi6.cu
@@ -116,14 +116,7 @@ void JacobiGPU( float* a, int n, int m, float w0, float w1, float w2, float tol 
 	da = dnewa;
 	dnewa = ta;  
    }while( change > tol );
-    printf( "JacobiGPU  converged in %d iterations to residual %f\n", iters, change );
-    printf( "JacobiGPU  used %f seconds total\n", sumtime/1000.0f );
-    cudaMemcpy( a, dnewa, memsize, cudaMemcpyDeviceToHost );
-    cudaFree( da );
-    cudaFree( dnewa );
-    cudaFree( lchange );
-    cudaEventDestroy( e1 );
-    cudaEventDestroy( e2 );
+    
     double time = sumtime/1000.0f;
     double dNumOps = 14.0 * iters * n *m;
     double gflops = dNumOps/time/1e9;
@@ -132,7 +125,15 @@ void JacobiGPU( float* a, int n, int m, float w0, float w1, float w2, float tol 
     printf( "JacobiGPU  converged in %d iterations to residual %f\n", iters, change );
     printf( "JacobiGPU  used %.5f seconds total\n", sumtime/1000.0f );
     printf( "Size(Number of Operations) = %.0f Ops/sec \n", dNumOps );
-    printf( "Throughtput = %.4f GFlops/sec \n",gflops );}
+    printf( "Throughtput = %.4f GFlops/sec \n",gflops );
+
+    cudaMemcpy( a, dnewa, memsize, cudaMemcpyDeviceToHost );
+    cudaFree( da );
+    cudaFree( dnewa );
+    cudaFree( lchange );
+    cudaEventDestroy( e1 );
+    cudaEventDestroy( e2 );
+    }
 
 static void init( float* a, int n, int m )
 {
