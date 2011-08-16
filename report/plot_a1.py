91d18441023c10eadca147e5aff6ea03b8e0f5b7
Torben Rasmussen <Torben.Rasmussen@latticesemi.com>
2011-08-15 21:21:38 -0700

    made all of the plot scripts work

--- report/plot_a1.py
+++ report/plot_a1.py
@@ -4,24 +4,18 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import csv
 
-files = ['data/1kmod_b10.csv',
-'data/1kmod_b14.csv',
-'data/1kmod_b16.csv',
-'data/1kmod_b20.csv',
-'data/j5_1k_b16.csv',
-'data/j5_original_b16.csv',
-'data/j6_1k_b16.csv',
-'data/j6_original_b16.csv',
-'data/j6mod_b10.csv',
-'data/j6mod_b14.csv',
-'data/j6mod_b16.csv',
-'data/j6mod_b20.csv']
-
 colors = ['r', 'g', 'b', 'c', 'm', 'k']
 
-############### LOOP 1 ##############
-fig1 = plt.figure(figsize=(10,10))
-ax1 = fig1.add_subplot(111)
+files = ['data/j5_original_16.csv',
+'data/j6_original_16.csv',
+'data/j6mod_10.csv',
+'data/j6mod_14.csv',
+'data/j6mod_16.csv',
+'data/j6mod_20.csv']
+
+############### LOOP ##############
+fig = plt.figure()
+ax = fig.add_subplot(111)
 
 i = 0
 for file in files:
@@ -36,6 +30,8 @@ for file in files:
     total_time = []
     memory_throughput = []
     instruction_throughput = []
+    gflops = []
+    occupancy = []
 
     for row in reader:
         matrix_size.append(float(row['Matrix Size']))
@@ -46,14 +42,22 @@ for file in files:
         total_time.append(float(row['Total Time']))
         #memory_throughput.append(float(row['Memory Throughput']))
         #instruction_throughput.append(float(row['Instruction Throughput']))
-    ax1.scatter(matrix_size, gpu_time, marker='o', c=colors[i%6], alpha=0.75)
+        gflops.append(float(row['GFLOPS']))
+        #occupancy.append(float(row['Occupancy']))
+
+    ax.scatter(matrix_size, gpu_time, marker='o', c=colors[i%6], alpha=0.75)
     i += 1
 
-ax1.set_xlabel('Matrix Size', fontsize=20)
-ax1.set_ylabel('Grid Size', fontsize=20)
+ax.set_xlabel('Matrix Size', fontsize=20)
+ax.set_ylabel('GPU time take (s)', fontsize=20)
+ax.set_title('GPU time taken vs Array Size')
+ax.grid(True)
 
-ax1.set_title('1 Kernel: Block size = 10')
-ax1.grid(True)
+ax.set_xlabel('Matrix Size', fontsize=20)
+ax.set_ylabel('GPU time taken (s)', fontsize=20)
+ax.set_title('GPU time taken vs Array Size')
+ax.grid(True)
+
 plt.legend( (files), loc=2, borderaxespad=0. )
 #####################################
 
---
+++ report/plot_d.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import csv
+
+colors = ['r', 'g', 'b', 'c', 'm', 'k']
+
+files = ['data/1kmod_10.csv', #THIS SHOULD BE SEQUENTIAL
+'data/1kmod_14.csv',
+'data/1kmod_16.csv',
+'data/1kmod_20.csv',
+'data/j5_1k_16.csv',
+'data/j5_original_16.csv',
+'data/j6_1k_16.csv',
+'data/j6_original_16.csv',
+'data/j6mod_10.csv',
+'data/j6mod_14.csv',
+'data/j6mod_16.csv',
+'data/j6mod_20.csv']
+
+############### LOOP ##############
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+i = 0
+for file in files:
+    print "parsing ", file
+    reader = csv.DictReader(open(file, 'rb'), delimiter='\t', quotechar='"')
+
+    matrix_size = []
+    grid_size = []
+    iterations = []
+    residual = []
+    gpu_time = []
+    total_time = []
+    memory_throughput = []
+    instruction_throughput = []
+    gflops = []
+    occupancy = []
+
+    for row in reader:
+        matrix_size.append(float(row['Matrix Size']))
+        grid_size.append(float(row['Grid Size']))
+        iterations.append(float(row['No of iterations']))
+        residual.append(float(row['Residual']))
+        gpu_time.append(float(row['GPU Time Taken']))
+        total_time.append(float(row['Total Time']))
+        #memory_throughput.append(float(row['Memory Throughput']))
+        #instruction_throughput.append(float(row['Instruction Throughput']))
+        gflops.append(float(row['GFLOPS']))
+        #occupancy.append(float(row['Occupancy']))
+
+    ax.scatter(matrix_size, gflops, marker='o', c=colors[i%6], alpha=0.75)
+    i += 1
+
+ax.set_xlabel('Matrix Size', fontsize=20)
+ax.set_ylabel('GFLOPS', fontsize=20)
+ax.set_title('GFLOPS vs Array Size')
+ax.grid(True)
+
+plt.legend( (files), loc=4, borderaxespad=0. )
+#####################################
+
+plt.show()
